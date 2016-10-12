21c96c382000d3aab98ce820a075d23a7577ceea
Tom <mail2tommy@gmail.com>
2016-10-12 15:54:33 +0200

    Add SerialPortNotifier
    Simple windows systemtray-tool to which notifies you when new
    (virtual)comport becomes available.
    Also show all available comports.

---
+++ SerialPortNotifier_C#/SerialPortNotifier/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SerialPortNotifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NI = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showActivePortsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NI
+            // 
+            this.NI.ContextMenuStrip = this.contextMenuStrip2;
+            this.NI.Icon = ((System.Drawing.Icon)(resources.GetObject("NI.Icon")));
+            this.NI.Text = "SerialMonitortor";
+            this.NI.Visible = true;
+            this.NI.Click += new System.EventHandler(this.NI_Click);
+            this.NI.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NI_MouseClick);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showActivePortsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(195, 84);
+            this.contextMenuStrip2.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip2_Opening);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // showActivePortsToolStripMenuItem
+            // 
+            this.showActivePortsToolStripMenuItem.Name = "showActivePortsToolStripMenuItem";
+            this.showActivePortsToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.showActivePortsToolStripMenuItem.Text = "ShowActivePorts";
+            this.showActivePortsToolStripMenuItem.Click += new System.EventHandler(this.showActivePortsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem whatToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon NI;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showActivePortsToolStripMenuItem;
+    }
+}
+
---
+++ SerialPortNotifier_C#/SerialPortNotifier/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPortNotifier
+{
+    public partial class Form1 : Form
+    {
+        private const int VisibleTime_mSec = 500;
+        List<string> listSerialPorts;
+        DateTime dt;
+        public Form1()
+        {
+            InitializeComponent();
+            listSerialPorts = new List<string>();
+
+            NI.BalloonTipText = "No updates";
+            NI.BalloonTipTitle = "Serial monitor";
+            NI.Visible = true;
+
+            dt = DateTime.Now;
+            var ports = SerialPort.GetPortNames().OrderBy(name => name);
+            foreach (var portName in ports)
+            {
+                listSerialPorts.Add(portName);
+            }
+            Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+
+        void showLastMessage()
+        {
+            if (NI.BalloonTipText != "")
+            {
+                NI.Visible = true;
+                NI.ShowBalloonTip(VisibleTime_mSec);
+            }
+        }
+
+        void RefreshMessage()
+        {
+            if (dt.AddMilliseconds(VisibleTime_mSec) < DateTime.Now)
+            {
+                NI.BalloonTipText = "";
+            }
+
+            dt = DateTime.Now;
+            List<string> newList = new List<string>();
+
+            var ports = SerialPort.GetPortNames().OrderBy(name => name);
+            foreach (var portName in ports)
+            {
+                newList.Add(portName);
+                if (listSerialPorts.Contains(portName) == false)
+                {
+                    NI.BalloonTipText += "\n+" + portName;
+                }
+                else
+                {
+                    listSerialPorts.Remove(portName);
+                }
+            }
+            foreach (string s in listSerialPorts)
+            {
+                NI.BalloonTipText += "\n-" + s;
+            }
+
+            listSerialPorts = newList;
+
+/*            if (NI.BalloonTipText == "")
+            {
+                NI.BalloonTipText = "No Updates";
+            }*/
+
+            showLastMessage();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 537: //WM_DEVICECHANGE
+                    RefreshMessage();
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        private void NI_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void NI_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    showLastMessage();
+                    break;
+
+                case MouseButtons.Right:
+                    contextMenuStrip2.Show();
+                    break;
+            }
+        }
+
+        private void contextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            const int delay = 1000;
+            NI.BalloonTipText = "Bye!";
+            NI.ShowBalloonTip(delay);
+            Thread.Sleep(delay);
+            Close();
+        }
+
+        private void showActivePortsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NI.BalloonTipText = "Active ports:";
+            var ports = SerialPort.GetPortNames().OrderBy(name => name);
+            foreach (var portName in ports)
+            {
+                NI.BalloonTipText += "\n " + portName;
+            }
+            showLastMessage();
+            NI.BalloonTipText = "";
+
+        }
+    }
+}
